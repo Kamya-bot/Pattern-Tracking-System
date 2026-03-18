30ffacbfa7d90e160c3491009b3d6ffd1c05e36a
Kamya Rawat <rawatkamya99@gmail.com>
2026-03-18 15:02:41 +0530

    Add GitHub Pages to CORS origins

--- config.py
+++ config.py
@@ -21,6 +21,7 @@ class Config:
         'http://127.0.0.1:8000',
         'http://localhost:5500',  # Live Server
         'http://127.0.0.1:5500',
+        'https://kamya-bot.github.io',
     ]
 
     # Session settings
